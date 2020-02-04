c92ed7c83e76da09ead9834a07a7eae1d850ea8e
Adoen Lunnae <unshekdelmonton@gmail.com>
2020-02-04 13:31:17 +0100

    Restructured the repo, started backend implementation

---
+++ P3/pw/WEB-INF/src/pw/ExperienceBean.java
@@ -0,0 +1,47 @@
+package pw;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExperienceBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Date start, end;
+	private String nombre, descripcion;
+	
+	public ExperienceBean() { }
+	
+	public ExperienceBean(Date start, Date end, String nombre, String descripcion) {
+		super();
+		this.start = start;
+		this.end = end;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
